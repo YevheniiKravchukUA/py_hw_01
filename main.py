591e1061084df0a679248cbaaba6f93d041705a1
Yevhenii Kravchuk <kravchukmail23@gmail.com>
2024-03-24 20:49:29 +0100

    fix task 4, change wrong dates

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from task_3 import normalize_phone
 from task_4 import get_upcoming_birthdays
 
 users = [
-    {"name": "John Doe", "birthday": "1985.01.23"},
-    {"name": "Jane Smith", "birthday": "1990.01.27"},
-    {"name": "Jane 1", "birthday": "1990.05.29"},
-    {"name": "Jane 2", "birthday": "1990.10.4"},
-    ]
+{"name": "Derek Dark", "birthday": "1985.03.16"},
+            {"name": "Jane Smith", "birthday": "1990.03.17"},
+            {"name": "Liam Smith", "birthday": "1995.03.18"},
+            {"name": "Mohel Smith", "birthday": "1995.03.19"},
+            {"name": "John Dark", "birthday": "1985.03.20"},
+            {"name": "Mary Dark", "birthday": "1985.03.21"},
+            {"name": "Derek Dark", "birthday": "1985.03.22"},
+            {"name": "Jane Smith", "birthday": "1990.03.23"},
+            {"name": "Nick Darsel", "birthday": "1984.03.24"},
+            {"name": "Jake Smith", "birthday": "1990.03.25"},
+            {"name": "Jane Smith", "birthday": "1990.03.26"},
+            {"name": "Nick Darsel", "birthday": "1984.03.27"},
+            {"name": "Jake Smith", "birthday": "1990.03.28"}]
 
 print(f"{get_days_from_today("2024,03,15")} days")
-print(f"{get_numbers_ticket(1, 1000, 10)}")
+print(f"{get_numbers_ticket(1, 1000, 6)}")
 print(normalize_phone("38(050)()-=**-wefsdf151\\\n,19:73"))
 print(get_upcoming_birthdays(users))
--- task_4.py
+++ task_4.py
@@ -7,26 +7,32 @@ def get_upcoming_birthdays(users):
     
     for user in users:
         user_data = {}
+        next_week_greetings = []
         
         user_date = datetime.datetime.strptime(user["birthday"], "%Y.%m.%d").date()
         now_date = datetime.datetime.today().date()
         user_birthday = datetime.datetime(now_date.year, user_date.month, user_date.day).date()
         user_data["name"] = user.get("name")
         
+        
         if user_birthday < now_date:
-            user_data["congratulation_date"] = (datetime.datetime(user_birthday.year + 1, user_birthday.month, user_birthday.day) + datetime.timedelta(days = 7))
+            user_data["congratulation_date"] = (datetime.datetime(user_birthday.year + 1, user_birthday.month, user_birthday.day)).date()
         else:
-            user_data["congratulation_date"] = (user_birthday + datetime.timedelta(days = 7))
+            user_data["congratulation_date"] = user_birthday
         
         greeting_dates.append(user_data)
 
     for greeding_date in greeting_dates:
-        match greeding_date.get("congratulation_date"):
+        if greeding_date.get("congratulation_date") >= now_date and greeding_date.get("congratulation_date") <= now_date + datetime.timedelta(days = 7):
+            next_week_greetings.append(greeding_date)
+        
+        match greeding_date.get("congratulation_date").weekday():
             case 5:
-                greeding_date.get("congratulation_date") + datetime.timedelta(days = 2)
+                greeding_date["congratulation_date"] = greeding_date.get("congratulation_date") + datetime.timedelta(days = 2)
             case 6:
-                greeding_date.get("congratulation_date") + datetime.timedelta(days = 1)
+                greeding_date["congratulation_date"] = greeding_date.get("congratulation_date") + datetime.timedelta(days = 1)
                 
         greeding_date["congratulation_date"] = greeding_date.get("congratulation_date").strftime("%Y.%m.%d")
 
+    print(f"Greetings dates for next 7 days: {next_week_greetings}")
     return greeting_dates
